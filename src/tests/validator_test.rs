94128729d7312a5bbdf3b69210b351afad2af797
Gabriel Ami <gabriel@simpleg.eu>
2022-07-28 16:19:53 +0200

    refactor: clarify function usage

--- src/tests/validator_test.rs
+++ src/tests/validator_test.rs
@@ -1,17 +1,17 @@
-use crate::validator::validate;
+use crate::validator::is_json_valid;
 
 #[cfg(test)]
 #[test]
 fn error_invalid_json() {
-    assert_eq!(false, validate("{abcd"));
+    assert_eq!(false, is_json_valid("{abcd"));
 }
 
 #[test]
 fn empty_is_not_valid() {
-    assert_eq!(false, validate(""));
+    assert_eq!(false, is_json_valid(""));
 }
 
 #[test]
 fn success_valid_json() {
-    assert_eq!(true, validate("{\"abcd\": 2}"));
+    assert_eq!(true, is_json_valid("{\"abcd\": 2}"));
 }
