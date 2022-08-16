fa4e92a423fc504a70c1e83fe755c314871e55cf
Gabriel Ami <gabriel@simpleg.eu>
2022-08-16 14:49:01 +0300

    doc: insight towards utility of modules

--- src/categories/category_id_tracker_io.rs
+++ src/categories/category_id_tracker_io.rs
@@ -2,6 +2,7 @@ use std::{collections::HashMap, io::Error};
 
 use crate::categories::category_id_tracker::CategoryEntry;
 
+/// Basic utilities for interacting with the category ID tracking storage.
 pub trait CategoryIdTrackerIO {
     fn read_entries(&self) -> Result<HashMap<String, CategoryEntry>, Error>;
     fn write_entry(&self, entry: &CategoryEntry) -> Result<(), Error>;
--- src/categories/category_io.rs
+++ src/categories/category_io.rs
@@ -2,6 +2,7 @@ use std::io::Error;
 
 use crate::categories::category::Category;
 
+/// Abstract wrapping for interacting with the storaging of categories.
 pub trait CategoryIO {
     fn read(&mut self) -> Result<Category, Error>;
     fn write(&self, category: &Category) -> Result<(), Error>;
