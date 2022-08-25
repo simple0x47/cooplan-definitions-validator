8e933c63fdc35ce68566b109ffd3ea14c9f4b9d0
Gabriel Ami <gabriel@simpleg.eu>
2022-08-25 12:46:55 +0300

    fix: grammar mistake

--- src/categories/category_io.rs
+++ src/categories/category_io.rs
@@ -2,7 +2,7 @@ use std::io::Error;
 
 use crate::categories::category::Category;
 
-/// Abstract wrapping for interacting with the storaging of categories.
+/// Abstract wrapping for interacting with the storing of categories.
 pub trait CategoryIO {
     fn read(&mut self) -> Result<Category, Error>;
     fn write(&self, category: &Category) -> Result<(), Error>;
