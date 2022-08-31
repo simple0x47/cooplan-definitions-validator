e4cd5c30d56462a4f44bf6b4220c52eb0a61b955
Gabriel Ami <gabriel@simpleg.eu>
2022-09-01 00:11:34 +0300

    test: selectable as last validation

--- src/tests/categories/validations/selectable_as_last_validation_test.rs
+++ src/tests/categories/validations/selectable_as_last_validation_test.rs
@@ -5,6 +5,7 @@ use std::rc::Rc;
 use crate::categories::category::Category;
 use crate::categories::validations::selectable_as_last_validation::SelectableAsLastValidation;
 use crate::categories::validations::validation::Validation;
+use crate::error::ErrorKind;
 
 #[test]
 fn error_if_not_selectable_as_last_and_has_no_children() {
@@ -64,4 +65,136 @@ fn detects_correct_category_correctly() {
     selectable_as_last_validation
         .validate(&[valid_category])
         .unwrap();
+}
+
+#[test]
+fn detects_correct_expanded_category_tree_correctly() {
+    use std::rc::Rc;
+
+    use crate::categories::category::Category;
+
+    let root_category = Category::new("root".to_string(), "root".to_string(), false, Vec::new());
+
+    let first_inner_category = Category::new_into_parent(
+        "inner 1".to_string(),
+        Rc::downgrade(&root_category),
+        "inner 1".to_string(),
+        false,
+        Vec::new(),
+    );
+
+    let second_inner_category = Category::new_into_parent(
+        "inner 2".to_string(),
+        Rc::downgrade(&(first_inner_category.unwrap())),
+        "inner 2".to_string(),
+        false,
+        Vec::new(),
+    )
+    .unwrap();
+
+    let third_inner_category = Category::new_into_parent(
+        "inner 3".to_string(),
+        Rc::downgrade(&second_inner_category),
+        "inner 3".to_string(),
+        true,
+        Vec::new(),
+    );
+
+    let forth_inner_category = Category::new_into_parent(
+        "inner 4".to_string(),
+        Rc::downgrade(&(third_inner_category.unwrap())),
+        "inner 4".to_string(),
+        false,
+        Vec::new(),
+    );
+
+    let third_alt_inner_category = Category::new_into_parent(
+        "inner 3 alt".to_string(),
+        Rc::downgrade(&second_inner_category),
+        "inner 3 alt".to_string(),
+        true,
+        Vec::new(),
+    );
+
+    let second_alt_inner_category = Category::new_into_parent(
+        "inner 2 alt".to_string(),
+        Rc::downgrade(&root_category),
+        "inner 2 alt".to_string(),
+        true,
+        Vec::new(),
+    );
+
+    let selectable_as_last_validation = SelectableAsLastValidation::new();
+
+    selectable_as_last_validation
+        .validate(&[root_category])
+        .unwrap();
+}
+
+#[test]
+fn error_if_there_is_a_non_selectable_last_category() {
+    use std::rc::Rc;
+
+    use crate::categories::category::Category;
+
+    let root_category = Category::new("root".to_string(), "root".to_string(), false, Vec::new());
+
+    let first_inner_category = Category::new_into_parent(
+        "inner 1".to_string(),
+        Rc::downgrade(&root_category),
+        "inner 1".to_string(),
+        false,
+        Vec::new(),
+    );
+
+    let second_inner_category = Category::new_into_parent(
+        "inner 2".to_string(),
+        Rc::downgrade(&(first_inner_category.unwrap())),
+        "inner 2".to_string(),
+        false,
+        Vec::new(),
+    )
+    .unwrap();
+
+    let third_inner_category = Category::new_into_parent(
+        "inner 3".to_string(),
+        Rc::downgrade(&second_inner_category),
+        "inner 3".to_string(),
+        true,
+        Vec::new(),
+    );
+
+    let forth_inner_category = Category::new_into_parent(
+        "inner 4".to_string(),
+        Rc::downgrade(&(third_inner_category.unwrap())),
+        "inner 4".to_string(),
+        false,
+        Vec::new(),
+    );
+
+    let third_alt_inner_category = Category::new_into_parent(
+        "inner 3 alt".to_string(),
+        Rc::downgrade(&second_inner_category),
+        "inner 3 alt".to_string(),
+        true,
+        Vec::new(),
+    );
+
+    let second_alt_inner_category = Category::new_into_parent(
+        "inner 2 alt".to_string(),
+        Rc::downgrade(&root_category),
+        "inner 2 alt".to_string(),
+        false,
+        Vec::new(),
+    );
+
+    let selectable_as_last_validation = SelectableAsLastValidation::new();
+
+    assert_eq!(
+        ErrorKind::LastCategoryNotSelectable,
+        selectable_as_last_validation
+            .validate(&[root_category])
+            .unwrap_err()
+            .kind()
+    );
 }
